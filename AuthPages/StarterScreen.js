a9258bc14c263da76a0c2d8bcce1feed6f18a31a
mirdanish8767@gmail.com <mirdilber43@gmail.com>
2025-08-29 14:40:35 +0530

    added auth folder

---
+++ AuthPages/StarterScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StarterScreen = () => {
+  return (
+    <View>
+      <Text>StarterScreen</Text>
+    </View>
+  )
+}
+
+export default StarterScreen
